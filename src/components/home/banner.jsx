d6b2db632b3a3f98fbdfdf142e30c72fd8a33b94
Md Aqil <aqil@wipl.co.in>
2021-02-14 22:23:28 +0530

    light and dark

--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -1,8 +1,10 @@
-import BannerImage from './banner-image'
 import Search from './search'
-import classes from './banner.module.css'
+import classes from './banner.module.css';
+import TravelArtLightIcon from "./travel-art.svg";
+import { useTheme } from '../../theme';
 
 export default function Banner() {
+   
     return (
         <div className={classes.heroSection}>
             <div className={classes.titleSection}>
@@ -10,7 +12,7 @@ export default function Banner() {
                 <h4 className={classes.subHeading}>100+ Travel Experiences in 12+ Countries</h4>
             </div>
             <Search />
-            <BannerImage />
+            <TravelArtLightIcon className={classes.icon} />
         </div>
     )
 }
--- src/components/layouts/app.jsx
+++ src/components/layouts/app.jsx
@@ -1,5 +1,19 @@
+import { useTheme } from "../../theme";
 import header from "./header.module.css";
 const Layout = (props)=> {
+    const colors = {
+        'light': 'Dark',
+        'dark': 'Light'
+    }
+    const theme = useTheme();
+    const handleChangeTheme = () => {
+        theme.changeColor(theme.color === 'dark' ? 'light' : 'dark')
+        if (document.body.classList.contains('dark')) {
+            document.body.classList.remove('dark')
+        } else {
+            document.body.classList.add('dark')
+        }
+    }
     return (
         <div>
             <div className="container">
@@ -14,8 +28,10 @@ const Layout = (props)=> {
                             <li><a href="">Destinations</a></li>
                             <li><a href="">Holiday Ideas</a></li>
                             <li><a href="">Packages</a></li>
+                            <li><button className={header.changeColor} onClick={handleChangeTheme}>{ colors[theme.color] }</button></li>
                         </ul>
                     </nav>
+                 
                 </header>
             </div>
             {props.children}
